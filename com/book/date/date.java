378ed78ade09da492fb08119e26ee73513ffc546
何龙 <helong_yg@yun-gui.com>
2016-10-10 18:43:14 +0800

    book

---
+++ com/book/date/date.java
@@ -0,0 +1,9 @@
+package com.book.date;
+
+import java.util.Date;
+
+public class date {
+	public static void main(String[] args) {
+		System.out.printf("%s %2$tb %2$te,%2$ty", "Due Date", new Date());
+	}
+}
